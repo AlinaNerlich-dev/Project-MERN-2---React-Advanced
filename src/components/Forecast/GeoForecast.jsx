e99d414759453f8d7d579f591feb031a578fb907
Alina <info@alina-nerlich.com>
2024-05-19 21:47:59 +0200

    fix forecast

--- src/components/Forecast/GeoForecast.jsx
+++ src/components/Forecast/GeoForecast.jsx
@@ -4,7 +4,8 @@ import { kelvinToCelcius } from "../functions";
 
 const GeoForecast = (data) => {
   const forecastData = data.data;
-
+console.log(forecastData)
+    /// Obj -> Array
   let flattenArr = [];
   for (let i = 0; i < forecastData.length; i++) {
     const {
@@ -14,33 +15,29 @@ const GeoForecast = (data) => {
     } = forecastData[i];
     flattenArr.push({ dt_txt, temp, icon });
   }
+  console.log(flattenArr)
 
-  const celciusArray = flattenArr.map((timestamp) => ({
+  // Convert Array into usable formats
+  const formatArray = flattenArr.map((timestamp) => ({
+    ... timestamp,
     dt_txt: timestamp.dt_txt.slice(0, 10),
     temp: kelvinToCelcius(timestamp.temp),
-    icon: timestamp.icon,
   }));
+  console.log(formatArray)
 
-  const finalArry = celciusArray.slice(1, 5);
+ // Group same days as Object
+  const grouped = Object.groupBy(formatArray, ({ dt_txt }) => dt_txt);
+  console.log(grouped)
 
-  const grouped = Object.groupBy(celciusArray, ({ dt_txt }) => dt_txt);
+  // Get the middle Object of each group/day
+  const singleDayInfo = Object.keys(grouped).map(el =>(grouped[el][4])).slice(1);
+  console.log(singleDayInfo)
 
-  function avgByGroup(groupedObj) {
-    const result = {};
-    for (const key of Object.keys(groupedObj)) {
-      const group = groupedObj[key];
-      result[key] =
-        group.reduce((avg, current) => avg + current.temp, 0) / group.length;
-    }
-    return result;
-  }
-
-  avgByGroup(grouped);
 
   return (
     <>
       <ul>
-        {finalArry.map((day) => {
+        {singleDayInfo.map((day) => {
           return (
             <li key={day.dt_txt}>
               <ForecastCard day={day} />
